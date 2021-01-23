7fab526bdc40ec21d37f9eb267c7fb41bc711ae7
Sarthak Agarwal <70764566+5arthak@users.noreply.github.com>
2021-01-24 02:57:12 +0530

    Add files via upload

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your portfolio'"; //letter subject
+	$to = 'sarthakyogesh@gmail.com'; // Enter Your E-mail
+	$from = 'sarthakwebdeveloper@gmail.com'; // Admin e-mail
+	$fromName = 'Sarthaks Portfolio'; // Your company name
+	$charset = 'UTF-8';
+?>
