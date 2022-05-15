d5528cd298a78fcfe9f3da37603674cab18e0188
5arthak <sarthaka@buffalo.edu>
2022-05-15 01:26:30 -0400

    Resume needs to be fixed/ Besides that portfolio neeeds project addition/Blog needs to be better

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "New message from your portfolio'"; //letter subject
-	$to = 'sarthakyogesh@gmail.com'; // Enter Your E-mail
-	$from = 'sarthakwebdeveloper@gmail.com'; // Admin e-mail
-	$fromName = 'Sarthaks Portfolio'; // Your company name
-	$charset = 'UTF-8';
-?>
